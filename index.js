830c37756e1b4402eb2d1d28ce0e6ce5b9612e93
Maigwa164 <davidmaigwa164@gmail.com>
2024-01-15 10:41:02 +0300

    Adding destination pages

--- index.js
+++ index.js
@@ -64,6 +64,7 @@ function carousel() {
   var x = document.getElementsByClassName("mySlides");
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";
+
   }
   slideIndex++;
   if (slideIndex > x.length) {slideIndex = 1}
