09debbfcec7c75953d2af45404e232b9a0c8303a
Maigwa164 <davidmaigwa164@gmail.com>
2024-01-15 15:02:38 +0300

    modifying the sliding image to fit

--- index.js
+++ index.js
@@ -64,10 +64,9 @@ function carousel() {
   var x = document.getElementsByClassName("mySlides");
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";
-
   }
   slideIndex++;
   if (slideIndex > x.length) {slideIndex = 1}
-  x[slideIndex-1].style.display = "block";
+  x[slideIndex-1].style.display = "flex";
   setTimeout(carousel, 5000); // Change image every 2 seconds
 }
