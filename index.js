994b2e8f8b88cbc221148647c37adb97afcc2b62
Maigwa164 <davidmaigwa164@gmail.com>
2024-01-23 13:32:06 +0300

    adding style to the about page

--- index.js
+++ index.js
@@ -42,18 +42,6 @@ function closeDropdown() {
     dropdownContent.classList.remove('show');
 }
 
-// Close dropdown when clicking outside of it
-window.onclick = function (event) {
-    if (!event.target.matches('.dropbtn')) {
-        var dropdowns = document.getElementsByClassName("dropdown-content");
-        for (var i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
-            }
-        }
-    }
-}
 
 //SLIDING IMAGE
 var slideIndex = 0;
